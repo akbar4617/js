f6d47a1bbf0b7c74011ebe58fe364705d542300d
akbar4617 <169093050+akbar4617@users.noreply.github.com>
2024-05-30 08:49:37 +0000

    console table pr

---
+++ jsbasic/basic.js
@@ -0,0 +1,7 @@
+console.log("Akbar Khan");
+const accountId = 12321;
+var accountName = "Akbar";
+let accountPass = "121";
+ accountCity = "indore"
+console.log(accountId);
+console.table([accountId,accountName,accountPass,accountCity]);
